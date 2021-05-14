afd4674cdfadf42a9ba24f28c559851b5c85a69c
vietanh1408 <you@example.com>
2021-05-14 19:16:51 +0700

    update port

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express()
 const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI,
+        await mongoose.connect(process.env.MONGO_ONLINE,
             {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
