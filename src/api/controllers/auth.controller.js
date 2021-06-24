7737d853133918dbe3f2344addfcc2bd94eb8d1c
vietanh1408 <you@example.com>
2021-06-24 15:12:06 +0700

    update

--- src/api/controllers/auth.controller.js
+++ src/api/controllers/auth.controller.js
@@ -1,69 +1,81 @@
-const User = require('./../../models/user.model')
-const validate = require('./../validations/auth.validation')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const User = require("./../../models/user.model");
+const validate = require("./../validations/auth.validation");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 module.exports.register = async (req, res) => {
-    // validate register
-    const { error } = validate.validateRegister(req.body)
-    if (error) return res.status(400).send({ error: error.details[0].message })
+  // validate register
+  const { error } = validate.validateRegister(req.body);
+  if (error) return res.status(400).send({ error: error.details[0].message });
 
-    // check email already exist
-    const emailExist = await User.findOne({ email: req.body.email })
-    if (emailExist) return res.status(400).send({
-        error: 'Email đã được sử dụng'
-    })
+  // check email already exist
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist)
+    return res.status(400).send({
+      error: "Email đã được sử dụng",
+    });
 
-    // security password
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+  // security password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        phone: req.body.phone,
-        password: hashedPassword
-    })
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    password: hashedPassword,
+  });
 
-    try {
-        const newUser = await user.save()
-        res.send({ user: newUser._id })
-
-    } catch (err) {
-        res.status(400).send({
-            error: "Tạo tài khoản không thành công"
-        })
-    }
-
-}
-
+  try {
+    const newUser = await user.save();
+    res.send({ user: newUser._id });
+  } catch (err) {
+    res.status(400).send({
+      error: "Tạo tài khoản không thành công",
+    });
+  }
+};
 
 module.exports.login = async (req, res, next) => {
+  //validate login
+  const { error } = validate.validateLogin(req.body);
+  if (error)
+    return res.status(400).send({
+      error: error.details[0].message,
+    });
 
-    //validate login
-    const { error } = validate.validateLogin(req.body)
-    if (error) return res.status(400).send(
-        {
-            error: error.details[0].message
-        }
-    )
+  // check user name valid in database
+  const user = await User.findOne({ email: req.body.email });
+  console.log("user..............", user);
+  if (!user) {
+    return res.status(400).send({
+      error: "Email không tồn tại",
+    });
+  }
 
-    // check user name valid in database
-    const user = await User.find({ name: req.body.name })
-    if (!user) return res.status(400).send(
-        {
-            error: "Tên đăng nhập không tồn tại"
-        }
-    )
+  // check password valid
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
 
-    // check password valid
-    const validPassword = bcrypt.compare(req.body.password, user.password)
-    if (!validPassword) return res.status(400).send({
-        error: "mật khẩu không đúng"
-    })
+  console.log("validPassword..........", validPassword);
+  if (!validPassword) {
+    return res.status(400).send({
+      error: "mật khẩu không đúng",
+    });
+  }
 
-    // create and assign a token
-    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET)
-    res.header('auth-token', token).send(token)
+  const currentUser = {
+    name: user.name,
+    email: user.email,
+    phone: user.phone,
+    avatar: user.avatar,
+  };
 
-}
+  // create and assign a token
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  //   res.header("auth-token", token).send(token);
+  const response = {
+    token,
+    user: currentUser,
+  };
+  res.json(response);
+};
--- src/api/validations/auth.validation.js
+++ src/api/validations/auth.validation.js
@@ -1,24 +1,28 @@
-const Joi = require('@hapi/joi')
+const Joi = require("@hapi/joi");
 
 module.exports.validateRegister = (data) => {
-    // schema
-    const schema = Joi.object({
-        name: Joi.string().required().min(6).max(255),
-        email: Joi.string().required().min(6).email(),
-        phone: Joi.string().required().min(9).max(255),
-        password: Joi.string().required().min(6).max(255).pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-        confirmPassword: Joi.ref('password'),
-    })
+  // schema
+  const schema = Joi.object({
+    name: Joi.string().required().min(6).max(255),
+    email: Joi.string().required().min(6).email(),
+    phone: Joi.string().required().min(9).max(255),
+    password: Joi.string()
+      .required()
+      .min(6)
+      .max(255)
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+    confirmPassword: Joi.ref("password"),
+  });
 
-    return schema.validate(data)
-}
+  return schema.validate(data);
+};
 
 module.exports.validateLogin = (data) => {
-    //schema
-    const schema = Joi.object({
-        name: Joi.string().required().min(6).max(255),
-        password: Joi.string().required().min(6).max(255)
-    })
+  //schema
+  const schema = Joi.object({
+    email: Joi.string().required().min(6).max(255).email(),
+    password: Joi.string().required().min(6).max(255),
+  });
 
-    return schema.validate(data)
-}
+  return schema.validate(data);
+};
