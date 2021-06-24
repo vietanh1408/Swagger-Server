dcf4f99049faf0d062abd9597eaf22de1ac08124
vietanh1408 <you@example.com>
2021-06-24 13:24:28 +0700

    update

--- src/api/controllers/product.controller.js
+++ src/api/controllers/product.controller.js
@@ -1,20 +1,21 @@
-const Product = require('./../../models/product.model')
-const ObjectId = require('mongodb').ObjectID
+const Product = require("./../../models/product.model");
+const ObjectId = require("mongodb").ObjectID;
 
 module.exports.index = async (req, res, next) => {
-    const products = await Product.find()
-    res.json(products)
-}
+  const products = await Product.find();
+  const total = await Product.count();
+  res.json({ products, total });
+};
 
 module.exports.search = async (req, res) => {
-    const keyword = req.query.keyword.toLowerCase().trim()
-    const name = new RegExp(keyword, 'g')
-    const matchProducts = await Product.find({ name: name })
-    res.json(matchProducts)
-}
+  const keyword = req.query.keyword.toLowerCase().trim();
+  const name = new RegExp(keyword, "g");
+  const matchProducts = await Product.find({ name: name });
+  res.json(matchProducts);
+};
 
 module.exports.detail = async (req, res) => {
-    const id = new ObjectId(req.params.id)
-    const product = await Product.findOne({ _id: id })
-    res.json(product)
-}
+  const id = new ObjectId(req.params.id);
+  const product = await Product.findOne({ _id: id });
+  res.json(product);
+};
--- src/index.js
+++ src/index.js
@@ -1,85 +1,87 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_ONLINE,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                useCreateIndex: true,
-                useFindAndModify: false
-            })
-        console.log('mongoDB connected')
-    } catch (err) {
-        console.log(err)
-    }
-}
-connectDB()
-const port = process.env.PORT || 6000
+  try {
+    await mongoose.connect(process.env.MONGO_ONLINE, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log("mongoDB connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
+connectDB();
+const port = process.env.PORT || 6000;
 
-const bodyParser = require("body-parser")
-const cookieParser = require('cookie-parser')
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const productRoute = require('./routes/product.route')
-const authRoute = require('./routes/auth.route')
-const userRoute = require('./routes/user.route')
-const cartRoute = require('./routes/cart.route')
+const cors = require("cors");
 
-const apiProductRoute = require('./api/routes/product.route')
-const apiUserRoute = require('./api/routes/user.route')
-const apiAuthRoute = require('./api/routes/auth.route')
+const productRoute = require("./routes/product.route");
+const authRoute = require("./routes/auth.route");
+const userRoute = require("./routes/user.route");
+const cartRoute = require("./routes/cart.route");
 
-const authMiddleware = require('./middlewares/auth.middleware')
-const sessionMiddleware = require('./middlewares/session.middleware')
+const apiProductRoute = require("./api/routes/product.route");
+const apiUserRoute = require("./api/routes/user.route");
+const apiAuthRoute = require("./api/routes/auth.route");
 
-app.set('view engine', 'pug')
-app.set('views', './src/views')
+const authMiddleware = require("./middlewares/auth.middleware");
+const sessionMiddleware = require("./middlewares/session.middleware");
 
-// Add headers
-app.use(function (req, res, next) {
+app.set("view engine", "pug");
+app.set("views", "./src/views");
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+app.use(cors());
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+// // Add headers
+// app.use(function (req, res, next) {
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Pass to next layer of middleware
-    next();
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+
+//     // Pass to next layer of middleware
+//     next();
+// });
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(express.static("public"));
+app.use(sessionMiddleware);
+
+app.use("/api/products", apiProductRoute);
+app.use("/api/users", apiUserRoute);
+app.use("/api/", apiAuthRoute);
+
+// app.use(authMiddleware.authMiddleware)
+
+app.use("/products", authMiddleware.authMiddleware, productRoute);
+app.use("/users", authMiddleware.authMiddleware, userRoute);
+app.use("/cart", authMiddleware.authMiddleware, cartRoute);
+app.use("", authRoute);
+
+app.get("/", authMiddleware.authMiddleware, (req, res) => {
+  res.render("index");
 });
 
-
-app.use(express.json())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser(process.env.COOKIE_SECRET))
-app.use(express.static('public'))
-app.use(sessionMiddleware)
-
-app.use('/api/products', apiProductRoute)
-app.use('/api/users', apiUserRoute)
-app.use('/api/', apiAuthRoute)
-
-app.use('/products', authMiddleware.authMiddleware, productRoute)
-app.use('/users', authMiddleware.authMiddleware, userRoute)
-app.use('/cart', authMiddleware.authMiddleware, cartRoute)
-app.use('', authRoute)
-
-app.get('/', authMiddleware.authMiddleware, (req, res) => {
-    res.render('index')
-})
-
-
-
 app.listen(port, () => {
-    console.log(`Server start at http://localhost:${port}`)
-})
+  console.log(`Server start at http://localhost:${port}`);
+});
