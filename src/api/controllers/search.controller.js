673acbb7e1989ed22938bbab26091435768a3d2f
vietanh1408 <you@example.com>
2021-06-26 23:07:27 +0700

    update

--- src/api/controllers/search.controller.js
+++ src/api/controllers/search.controller.js
@@ -4,16 +4,19 @@ const ObjectId = require("mongodb").ObjectID;
 module.exports.search = async (req, res) => {
   const pageIndex = parseInt(req.query.pageIndex);
   const pageSize = parseInt(req.query.pageSize);
-  const keyword = req.query.keyword.toLowerCase().trim();
-  const name = new RegExp(keyword, "g");
-  const matchProducts = await Product.find({ name: name });
-  const total = await Product.find({ name: name }).count();
-  if (pageIndex) {
-    let skip = (pageIndex - 1) * pageSize;
+  const keyword = req.query.keyword;
 
+  let skip = (pageIndex - 1) * pageSize;
+  if (keyword) {
+    const name = new RegExp(".*" + keyword.toLowerCase().trim() + ".*");
+    const total = await Product.find({ name: name }).count();
     const matchProducts = await Product.find({ name: name })
       .skip(skip)
       .limit(pageSize);
     res.json({ matchProducts, total });
-  } else res.json({ matchProducts, total });
+  } else {
+    const total = await Product.find({}).count();
+    const matchProducts = await Product.find({}).skip(skip).limit(pageSize);
+    res.json({ matchProducts, total });
+  }
 };
